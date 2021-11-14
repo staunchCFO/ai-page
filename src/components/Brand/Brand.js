09aea2eb4f851ba3a5120ca4348989100e02140d
Emeka Okezie <okezieemeka949@gmail.com>
2021-11-15 00:06:49 +0100

    Added the Brand component

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -1,12 +1,34 @@
 import React from 'react'
-import './Style.js'
+import Google from '../../assets/images/google.png'
+import Shopify from '../../assets/images/shopify.png'
+import Slack from '../../assets/images/slack.png'
+import Dropbox from '../../assets/images/dropbox.png'
+import Atlassian from '../../assets/images/atlassian.png'
+
+import { BrandContainer } from './Style.js'
 
 
 const Brand = () => {
     return (
-        <div>
-            Brand
-        </div>
+        <BrandContainer>
+            <div className="gpt3__brand section__padding">
+                <div>
+                    <img src={Google} alt="google" />
+                </div>
+                <div>
+                    <img src={Shopify} alt="shopify" />
+                </div>
+                <div>
+                    <img src={Slack} alt="slack" />
+                </div>
+                <div>
+                    <img src={Dropbox} alt="dropbox" />
+                </div>
+                <div>
+                    <img src={Atlassian} alt="Atlassian" />
+                </div>
+            </div>
+        </BrandContainer>
     )
 }
 
---
+++ src/components/Brand/Style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+
+export const BrandContainer = styled.div`
+
+    .gpt3__brand {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+
+        div {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex: 1;
+            max-width: 150px;
+            min-width: 120px;
+            margin: 1rem;
+        }
+    }
+
+`
